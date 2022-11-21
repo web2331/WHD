7dd79a374a00f1945195021606d66992caceb001
alizaman11 <89982843+alizaman11@users.noreply.github.com>
2022-11-21 16:36:57 +0000

    V4 - epc integration

--- app-v3/routes.js
+++ app-v3/routes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-const request = require('sync-request')
 // Add your routes here - above the module.exports line
 
 module.exports = router
@@ -57,7 +56,7 @@ router.post('/bill-name-validate', function (req, res) {
 
   var name = req.session.data['name-on-bill']
 
-  if (name == "your name" || name == "your partner's name" || name === "Someone who deals with the Department for Work and Pensions on your behalf (your 'appointee')") {
+  if (name == "your name" || name == "your partner's name" || name === "My Department of Work and Pension's (DWP) Appointee") {
     res.redirect('/electricity-supplier')
   } if (name == "your landlord's name") {
     res.redirect('/ineligible-bill-name-landlord')
@@ -86,47 +85,14 @@ router.post('/benefits-validate', function (req, res) {
 
   if (data == "none") {
     res.redirect('/ineligible-benefits')
-  } if (data == "The 'Guarantee Credit' part of Pension Credit") {
+  } if (data == "The ‘Guarantee Credit’ part of Pension Credit ") {
     res.redirect('/eligible-letter-wait')
   } else if ( checker(["Child Tax Credit", "Working Tax Credit"], data) ) {
     res.redirect('/household-occupants')
   } else {
-    res.redirect('/postcode')
+    res.redirect('/property-type')
   }
 
-})
-
-router.post('/select-address', function (req, res) {
-  var data = req.session.data['postcode']
-
-  var url = "https://beis-epc-api-prototype.azurewebsites.net/api/GetCertificates?code=hKmrGolL_RbeDpithM3fMNcFOiJFfCcmLVC7NPSPXR01AzFuXOkRdQ==&postcode=" + data;
-  var fetch = request('GET', url);
-  var results = fetch.getBody()
-
-  const parseResults = (obj, prefix) => {
-    var arr = Object.entries(obj).reduce((acc, [key, value]) => {
-      acc.push({ address: value['address'] + ', ' + value['posttown'] + ', ' + data, type: value['built-form'], size: value['total-floor-area'], age: value['construction-age-band'].replace('England and Wales: ', '') });
-      return acc;
-    }, []);
-    return arr;
-  };
-
-  if ( results.length != 0 ) {
-    results = parseResults( JSON.parse( results).rows ).sort((a, b) => (a.address.localeCompare(b.address, undefined, { numeric: true })));
-  } else {
-    results = false;
-  }
- 
-  res.render('select-address.html', { postcodeResults: results })
-})
-
-router.get('/check-property-details', function (req, res) {
-  req.session.data['property-type'] = req.query.propertyType;
-  req.session.data['property-age'] = req.query.propertyAge;
-  req.session.data['property-size'] = req.query.propertySize;
-  req.session.data['postcodeLookup'] = true;
-  
-  res.render('check-property-details.html', { propertyDetails: { address: req.query.address, type: req.query.propertyType, age: req.query.propertyAge, size: req.query.propertySize } })
 })
 
 router.post('/income-validate', function (req, res) {
@@ -134,7 +100,7 @@ router.post('/income-validate', function (req, res) {
   var data = req.session.data['household-income']
 
   if (data <= 20000) {
-    res.redirect('/postcode')
+    res.redirect('/property-type')
   } else {
     res.redirect('/ineligible-income')
   }
@@ -156,7 +122,6 @@ router.post('/epc-validate', function (req, res) {
 router.post('/property-type-validate', function (req, res) {
 
   var data = req.session.data['property-type']
-  req.session.data['postcodeLookup'] = false;
 
   if (data == "Park (mobile) home") {
     res.redirect('/ineligible-property-parkhome')
@@ -171,15 +136,8 @@ router.post('/property-type-validate', function (req, res) {
 router.post('/answer-validate', function (req, res) {
 
   var data = req.session.data['property-type']
-  var lookup = req.session.data['postcodeLookup'];
-  
-  if ( (data == "Detached house" || data == "Detached") && ( lookup ) ) {
-    res.redirect('/eligible-unqualified-epc')
-  } else if ( lookup ) {
-    res.redirect('/eligible-qualified-epc')
-  }
 
-  if (data == "Detached house" || data == "Detached") {
+  if (data == "Deatached house") {
     res.redirect('/eligible-unqualified')
   } else {
     res.redirect('/eligible-qualified')
